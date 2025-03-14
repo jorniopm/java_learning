1888d62be160a1c86fae52b6e52fabed6b0822c5
wangjiacheng <2984458902@qq.com>
2025-03-14 14:02:24 +0800

    添加 Main.java 和 learn02.java 文件，包含示例代码

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,42 @@
 public class Main {
     public static void main(String[] args) {
+//        int i = 0 , sum = 0;
+//        do {sum = sum + i;
+//            i = i + 1;
+//        } while (i <= 100);
+//        System.out.println("sum="+sum);
+//  输出1+2+...+100
 
-        System.out.println("Hello, World!");
+//        int i = 1, count = 1;
+//        int sum = 0;
+//        for (i = 1; i <= 5; i++) {
+//            count = i * count;
+//            sum += count;
+//        }
+//        System.out.println(sum);
+//  输出1至5的阶乘
+//
+//        int i, j;
+//        for(i=1;i<=10;i++){
+//            for(j=1;j<=15 -i;j++) {
+//                System.out.print(" ");
+//            }
+//            for(j=1;j<=2*i-1;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//  输出星星阶梯
+
+//        OuterLoop:
+//        for (int i=2;i<=100;i++)
+//        {
+//            for (int j=2;j<i;j++)
+//            { if (i%j==0)
+//                continue OuterLoop;
+//            }
+//            System.out.println (i);
+//        }
+//  输出100以内的质数
+
     }
 }

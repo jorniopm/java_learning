9a78383b68469066a9a57a8a2321e8398640ceb4
wangjiacheng <2984458902@qq.com>
2025-03-14 14:03:41 +0800

    更新 Main.java 和 learn02.java，添加立方计算功能并注释掉示例代码

--- src/learn02.java
+++ src/learn02.java
@@ -1,5 +1,36 @@
 public class learn02 {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+//        System.out.println("Hello World");
+//冒泡排序
+//int[] arr = {64, 34, 25, 12, 22, 11, 90};
+//System.out.println("原始数组：");
+//for (int num : arr) {
+//    System.out.print(num + " ");
+//}
+//System.out.println();
+//
+//for (int i = 0; i < arr.length - 1; i++) {
+//    for (int j = 0; j < arr.length - 1 - i; j++) {
+//        if (arr[j] > arr[j + 1]) {
+//            int temp = arr[j];
+//            arr[j] = arr[j + 1];
+//            arr[j + 1] = temp;
+//        }
+//    }
+//}
+//
+//System.out.println("排序后数组：");
+//for (int num : arr) {
+//    System.out.print(num + " ");
+//}
+//
+        for (int i=0; i<6; i++){
+            System.out.println(i + "\t" + cube(i));
+        }
+
+    }
+        static int cube (int n){
+    return n*n*n;
     }
 }

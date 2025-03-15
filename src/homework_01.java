ccc379ab3aa545bb3ca670d92f0212ecf0e4c41a
wangjiacheng <2984458902@qq.com>
2025-03-15 13:06:06 +0800

    添加 homework_01.java 文件，包含用户输入和计算功能

--- src/homework_01.java
+++ src/homework_01.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class homework_01 {
+    public static void main(String[] args) {
+        //获取要求数字
+        Scanner sc = new Scanner(System.in);
+        System.out.println("输入一个10至20之间的数字: ");
+
+        int n;
+        int sum = 0;
+        do {n = sc.nextInt();
+            for (int i = 1; i <= n; i++){
+                int j = 1;
+                
+                sum = i/j;
+
+            }
+
+        } while (n < 10 || n > 20);
+        sc.close();
+        System.out.println(sum);
+    }
 }

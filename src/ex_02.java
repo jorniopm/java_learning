7acbb9e5af20f2c9cfb87bcfcf6913acc353b4e1
jorniopm <2984458902@qq.com>
2025-04-25 14:46:29 +0800

    实现 ex_02.java 中的斐波那契数列输出功能；完善 ex_03.java 中的递归计算和打印逻辑

--- src/ex_02.java
+++ src/ex_02.java
@@ -1,3 +1,20 @@
 public class ex_02 {
-    public static void main
+    public static void main(String[] args) {
+        int f1 = 1;
+        int f2 = 1;
+        int fn = 0;
+        System.out.print(f1 + " ");
+        System.out.print(f2 + " ");
+
+        while (true) {
+            fn = f1 + f2;
+            if (fn <= 100){
+                System.out.print(fn + " ");
+            }else{
+                break;
+            }
+            f1 = f2;
+            f2 = fn;
+        }
+    }
 }
--- src/ex_03.java
+++ src/ex_03.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class ex_03 {
+    public static int fibonacci(int n) {
+        if (n <= 1){
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+    public static void printFibonacci() {
+        int i = 1;
+        int result;
+        while(true) {
+            result = fibonacci(i);
+            if (result > 100){
+                break;
+            }
+            System.out.print(result + " ");
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("100以内的斐波那契数列为：");
+                printFibonacci();
+    }
 }

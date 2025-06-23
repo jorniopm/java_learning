c881653bfd1fccb04853ef1ca2751832c0a95ae3
jorniopm <2984458902@qq.com>
2025-06-23 21:50:19 +0800

    实现 FinalEx.java 文件，定义 Box 类及其体积计算功能

--- src/FinalEx.java
+++ src/FinalEx.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class FinalEx {
+    public static void main(String[] args) {
+        Box myBox1 = new Box(10, 20, 30);
+        Box myBox2 = new Box(3, 6, 9);
+        double vol;
+        vol = myBox1.volume();
+        System.out.println("myBox1 Volume is "+vol);
+        vol = myBox2.volume();
+        System.out.println("myBox2 Volume is "+vol);
+    }
+
+}
+
+class Box{
+    double width, height, depth;
+    Box(double w, double h, double d) {
+        this.width = w;
+        this.height = h;
+        this.depth = d;
+    }
+    double volume() {
+        return width * height * depth;
+    }
 }
